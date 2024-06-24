9062910dfe43f2ef4b637df7af7ba452cf8bfa34
Sandaru-kaushan <sandarukaushan1560@gmail.com>
2024-06-24 23:29:36 +0530

    first commit

---
+++ pr2q1.scala
@@ -0,0 +1,28 @@
+object Main  {
+var k,i,j=2
+
+var m,n=5
+
+var f=12.0f
+
+var g=4.0f
+
+var c='X'
+
+
+
+val resultA = k + 12 * m
+val resultB = m / j
+val resultC = n % j
+val resultD = m / j * j
+val resultE =( f + 10 * 5 + g)
+
+
+println(s"a: $resultA")
+println(s"b: $resultB")
+println(s"c: $resultC")
+println(s"d: $resultD")
+println(s"e: $resultE")
+println(s"++i*n --not working it is show error")
+}
+
